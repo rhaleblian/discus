c1de8c8d69cfdd7b98ed17f360347c013dc02501
Ray Haleblian <ray@haleblian.com>
2014-05-04 18:19:44 -0700

    Revert "Revert "Use new stylesheet.""
    This reverts commit cc0c3f91b0cd5a1ce43e61dd661be07c7d6e675e.

--- src/media/index.php
+++ src/media/index.php
@@ -1,28 +1,26 @@
-<!doctype html public '-//W3C//DTD HTML 4.01//EN'
-  'http://www.w3.org/TR/html4/strict.dtd'>
-<html>
+<!doctype html>
+<html lang="en">
   <head>
-    <link rel="stylesheet"
-    href="http://www.w3.org/StyleSheets/Core/Chocolate" 
-    type="text/css">
+    <link rel="stylesheet" href="/css/media.css" type="text/css">
+    <script type="text/javascript" src="retina.js"></script>
 <?php
 
-function echorow($row) {
+function echo_row($row) {
 	# print a row from the disc table.
 	$id = $row['id'];
 	$name = $row['name'];
 	$label = $row['label'];
 	$status = $row['status'];
 	if ($status > 0) echo "<s>"; 
-	echo "<a href=\"?disc_id=", $id, "\">";
+	echo "<a href=\"?disc_id=", $id, "\"><li><h2>";
 	if (strlen($name) && strlen($label) && ($name != $label)) 
-		echo $label, " [", $name, "]</a>";
+		echo $label, " [", $name, "]";
 	else if (strlen($label))
-		echo $label, "</a>";
+		echo $label;
 	else
-		echo $name, "</a>";
+		echo $name;
+	echo "</h2></li></a>";
 	if ($status > 0) echo "</s>";
-	echo "</br>\n";
 }
 
 $disc_id = $_GET['disc_id'];
@@ -39,12 +37,15 @@ $conn = mysql_connect($ini_array["host"],
 $result = mysql_select_db($ini_array["db"]);
 
 if ($disc_id == "") {
-    
     # Available discs.
 ?>
     <title>Optical Media Catalog</title>
   </head>
 <body>
+<div id="view">
+<header><h1>Discs</h1></header>
+<div id="container">
+<ul>
 <?php
 	$sql = "select id, label, name, status from disc";
 	if ($extant == 1) {
@@ -57,49 +58,39 @@ if ($disc_id == "") {
 		$rows[] = $row;
 	}
 
-	echo '<table border="1" cellpadding="8" cellspacing="8">';
-	echo '<tr>',"\n";
-
 	$columns=1;
 	$i=0;
 	for ($column=1; $column<=$columns; $column++) {
-		echo "<td>\n";
 		for (; $i<count($rows)*($column/$columns); $i++) {
-			echorow($rows[$i]);
+			echo_row($rows[$i]);
 		}
-		echo "</td>\n";
 	}
-
-	echo "</tr></table>\n";
-
+?>
+</ul>
+</div></div>
+<?php
 } else {
-    
 	# Disc contents.
 
 	$sql  = "select file.* from disc";
 	$sql .= " inner join file on disc.id=file.disc_id";
 	$sql .= " where disc.id = '" . $disc_id . "'";
 	$result = mysql_query($sql);
-	$lastdir = "";
+	$prev_dir = "";
 ?>
     <title>Optical Media Catalog</title>
   </head>
   <body>
-    <table cellpadding="8">
-      <tr><td>Folder</td><td>File</td></tr>
-<?php	
+    <div id="view">
+<?php
 	while ($row = mysql_fetch_assoc($result)) {
-		echo "<tr>";
-		if ($lastdir != $row['dir']) {
-			echo "<td>",$row['dir'],"</td>";
-		} else {
-			echo "<td></td>";
+		if ($prev_dir != $row['dir']) {
+			echo "[", $row['dir'], "]";
+    		$prev_dir = $row['dir'];
 		}
-		echo "<td>",$row['name'],"</td>";
-		echo "</tr>";
-		$lastdir = $row['dir'];
+		echo "<h2>", $row['name'], "</h2>";
 	}
-	echo "\n</table>\n";
+    echo "</div>";
 }
 ?>
   </body>
