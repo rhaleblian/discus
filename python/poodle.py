f2d3b4550badeaca1fda84df85354690d0673ff9
Ray Haleblian <ray@haleblian.com>
2014-12-30 10:53:22 -0800

    Add a scraper for Shoeboxed processing document filenames.

--- python/poodle.py
+++ python/poodle.py
@@ -1,24 +1,54 @@
-""" Talk to Google Data API to get cell values.
+""" Munging web services.
 """
+#import pprint
 
-import gdata.spreadsheets.client
-import simplejson
-import os
-import pprint
 
-conf_root = os.getenv('USERPROFILE') or os.getenv('HOME')
-conf_path = os.path.join(conf_root, 'config.json')
-conf = simplejson.load(open(conf_path))
-key = conf.get('spreadsheet_key')
+def gdata_cell_values():
+    """ Talk to Google Data API to get cell values.
+    """
+    import gdata.spreadsheets.client
+    import simplejson
+    import os
 
-client = gdata.spreadsheets.client.SpreadsheetsClient()
-token = client.ClientLogin(conf.get('user'), conf.get('password'), 'me')
-sheet = client.GetWorksheet(key, 1)
-cq = gdata.spreadsheets.client.CellQuery(3, 3, 1, 3)
-cs = client.GetCells(key, 1, q=cq)
-for i in range(0, 3):
-    cell_ent = cs.entry[i]
-    print cell_ent.cell.input_value  # get a current cell value
+    conf_root = os.getenv('USERPROFILE') or os.getenv('HOME')
+    conf_path = os.path.join(conf_root, 'config.json')
+    conf = simplejson.load(open(conf_path))
+    key = conf.get('spreadsheet_key')
+
+    client = gdata.spreadsheets.client.SpreadsheetsClient()
+    token = client.ClientLogin(conf.get('user'), conf.get('password'), 'me')
+    sheet = client.GetWorksheet(key, 1)
+    cq = gdata.spreadsheets.client.CellQuery(3, 3, 1, 3)
+    cs = client.GetCells(key, 1, q=cq)
+    for i in range(0, 3):
+        cell_ent = cs.entry[i]
+        print cell_ent.cell.input_value  # get a current cell value
+
+
+def skreeeeek():
+    """
+    Scrape HTML from Shoeboxed add documents page, to get full image filenames.
+    """
+    import os
+    import shutil
+    from xml.etree import cElementTree
+
+    home = os.getenv('USERPROFILE')
+    filename = home + '/Dropbox/Pictures/Receipt/web_snippet'
+
+    tree = cElementTree.parse(filename)
+    root = tree.getroot()
+    elements = root.findall('./ul/li/div')
+    names = [e.get('title') for e in elements if e.get('title') is not None]
+
+    def move(names):
+        d_src = home + '/Dropbox/Pictures/Receipt/'
+        d_dst = home + '/Dropbox/Pictures/Receipt/Processing/'
+        for n in names:
+            shutil.move(d_src + n, d_dst + n)
+
+    print(names)
+    #move(names)
 
 
 __author__ = 'rhaleblian'
