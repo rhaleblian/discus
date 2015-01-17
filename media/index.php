32f6bb982cd36924536193776bbd8947d269e68a
Ray Haleblian <ray@haleblian.com>
2015-01-17 13:42:42 -0800

    Add simple search.

--- media/index.php
+++ media/index.php
@@ -103,7 +103,8 @@ function search($term) {
     echo '<table class="table">';
     echo '<thead><tr><td>disc</td><td>file</td><td>folder</td></tr></thead>';
     echo '<tbody>';
-    $sql = "SELECT * FROM file_view WHERE name LIKE '%" . $term . "%' OR dir LIKE '%" . $term . "%';";
+    $sql = "SELECT * FROM entry WHERE name LIKE '%" . $term . "%' OR dir LIKE '%" . $term . "%' LIMIT 256;";
+    echo "<p>" . $sql . "</p>\n";
     $result = mysql_query($sql);
     $rows = array();
 	while ($row = mysql_fetch_assoc($result)) {
@@ -144,6 +145,65 @@ if (array_key_exists('extant', $_GET)) {
 <body class="body">
 <div class="container">
 
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="/media">Disc Catalog</a>
+    </div>
+    <!-- Collect the nav links, forms, and other content for toggling -->
+<!--
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+        <li><a href="#">Link</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+            <li class="divider"></li>
+            <li><a href="#">One more separated link</a></li>
+          </ul>
+        </li>
+      </ul>
+-->
+      <form class="navbar-form navbar-left" action="/media" method="get" role="search">
+        <div class="form-group">
+          <input type="text" class="form-control" name="search" placeholder="Search">
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+      </form>
+      <!--
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">Link</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+          </ul>
+        </li>
+      </ul>
+      -->
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+
+  <div class="col-md-12">
+
 <?php
 if ($term != "") {
     search($term);
@@ -173,6 +233,6 @@ else if ($disc_id == "") {
     echo_disc_contents_flat($disc_id);
 }
 ?>
-</div>
+</div></div>
 </body>
 </html>
