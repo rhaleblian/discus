0476354b7d09935a86825cf9c909fec7e2149f79
Ray Haleblian <ray@haleblian.com>
2016-05-15 20:31:20 +0000

    Indent HTML.

--- index.php
+++ index.php
@@ -31,33 +31,31 @@ if (array_key_exists('extant', $_GET)) {
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
     </head>
     
-<body class="body">
-<div class="container">
-
-<nav class="navbar navbar-default">
-  <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="<?php $baseurl ?>">Disc Catalog</a>
-    </div>
-
-      <form class="navbar-form navbar-left" action="<?php $basedir ?>" method="get" role="search">
-        <div class="form-group">
-          <input type="text" class="form-control" name="search" placeholder="">
-        </div>
-        <button type="submit" class="btn btn-default">Search</button>
-      </form>
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-</nav>
-
-<div class="col-md-12">
+    <body class="body">
+        <div class="container">
+        
+            <nav class="navbar navbar-default">
+                <div class="container-fluid">
+                    <!-- Brand and toggle get grouped for better mobile display -->
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="<?php $baseurl ?>">Disc Catalog</a>
+                    </div>
+                    <form class="navbar-form navbar-left" action="<?php $basedir ?>" method="get" role="search">
+                        <div class="form-group">
+                            <input type="text" class="form-control" name="search" placeholder="">
+                        </div>
+                        <button type="submit" class="btn btn-default">Search</button>
+                    </form>
+                </div><!-- /.navbar-collapse -->
+            </nav>
+    
+            <div class="col-md-12">
 <?php
 if ($term != "") {
     search($term);
@@ -65,31 +63,31 @@ if ($term != "") {
 else if ($disc_id == "") {
     # Available discs.
 ?>
-    <div class="col-md-6">
-        <h2>Discs</h2>
-            <table class="table">
-            <thead>
-                <tr>
-                <th>Printed Label</th>
-                <th>Volume Name</th>
-                <th>Disposition</th>
-                </tr>
-            </thead>
-            <tbody>
+                <div class="col-md-6">
+                    <h2>Discs</h2>
+                    <table class="table">
+                        <thead>
+                            <tr>
+                            <th>Printed Label</th>
+                            <th>Volume Name</th>
+                            <th>Disposition</th>
+                            </tr>
+                        </thead>
+                        <tbody>
 <?php echo_discs($extant); ?>
-            </tbody>
-        </table>
-    </div>
+                        </tbody>
+                    </table>
+                </div>
 <?php
 } else {
     echo_disc_contents($disc_id);
 }
 ?>
-</div>
-</div>
-
-<script src="https://code.jquery.com/jquery-2.2.3.min.js" integrity="sha256-a23g1Nt4dtEYOj7bR+vTu7+T8VP13humZFBJNIYoEJo=" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-
-</body>
+            </div>
+        </div><!-- /.container-fluid -->
+    
+        <script src="https://code.jquery.com/jquery-2.2.3.min.js" integrity="sha256-a23g1Nt4dtEYOj7bR+vTu7+T8VP13humZFBJNIYoEJo=" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+    
+    </body>
 </html>
